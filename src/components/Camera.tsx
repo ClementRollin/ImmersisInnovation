644ddd63c68764469f896c6d535329d1ca71a20e
ClementRollin <clemrollin01@gmail.com>
2024-10-11 09:15:06 +0200

    New changes

--- src/components/Camera.tsx
+++ src/components/Camera.tsx
@@ -1,18 +1,10 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { CameraType, CameraView, useCameraPermissions } from 'expo-camera';
-import { Button, StyleSheet, Text, View, TouchableOpacity, Alert, Linking } from 'react-native';
-import { GLView } from 'expo-gl';
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { Renderer } from 'expo-three';
-import { Asset } from 'expo-asset'; // Importation pour gérer les fichiers locaux
-import { ExpoWebGLRenderingContext } from 'expo-gl';
+import { Button, StyleSheet, Text, View, Image, TouchableOpacity, Alert, Linking } from 'react-native';
 
 const videoFiles = [
     'video.mp4',
-    'video - Copie.mp4',
-    'video - Copie (2).mp4',
-    'video - Copie (3).mp4'
+    'video - Copie.mp4'
 ];
 
 const Camera = () => {
@@ -21,8 +13,6 @@ const Camera = () => {
     const [socket, setSocket] = useState<WebSocket | null>(null);
     const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
     const [isVideoPlaying, setIsVideoPlaying] = useState(false);
-    const [modelLoaded, setModelLoaded] = useState<boolean>(false);
-    const mixerRef = useRef<THREE.AnimationMixer | null>(null);
 
     useEffect(() => {
         const ws = new WebSocket('ws://10.26.129.176:8085');
@@ -86,58 +76,6 @@ const Camera = () => {
         }
     };
 
-    const onContextCreate = async (gl: ExpoWebGLRenderingContext) => {
-        const renderer = new Renderer({ gl });
-        renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
-
-        const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera(75, gl.drawingBufferWidth / gl.drawingBufferHeight, 0.1, 1000);
-        camera.position.set(0, 0, 2);
-
-        const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 4);
-        scene.add(light);
-
-        const loader = new GLTFLoader();
-
-        const modelAsset = Asset.fromModule(require('../../assets/model4.glb'));
-
-        await modelAsset.downloadAsync();
-
-        loader.load(
-            modelAsset.localUri || '',
-            (gltf) => {
-                const model = gltf.scene;
-                scene.add(model);
-
-                const mixer = new THREE.AnimationMixer(model);
-                mixer.clipAction(gltf.animations[0]).play();
-                mixerRef.current = mixer;
-
-                model.position.set(0, -2, -2);
-                model.scale.set(0.5, 0.5, 0.5);
-
-                setModelLoaded(true);
-            },
-            undefined,
-            (error) => {
-                console.error('Erreur lors du chargement du modèle :', error);
-            }
-        );
-
-        const clock = new THREE.Clock();
-
-        const render = () => {
-            const delta = clock.getDelta();
-            if (mixerRef.current) mixerRef.current.update(delta);
-
-            renderer.render(scene, camera);
-            gl.endFrameEXP();
-            requestAnimationFrame(render);
-        };
-
-        render();
-    };
-
     if (!permission) {
         return <View />;
     }
@@ -152,25 +90,31 @@ const Camera = () => {
     }
 
     return (
-        <View style={styles.container}>
-            <View style={styles.cameraContainer}>
-                <CameraView style={styles.camera} facing={facing} />
-                <View style={styles.gifContainer}>
-                    {isVideoPlaying ? (
-                        <Text style={styles.playingMessage}>Lecture vidéo en cours...</Text>
-                    ) : (
-                        <GLView style={styles.glView} onContextCreate={onContextCreate} />
-                    )}
-                </View>
+        <View style={styles.cameraContainer}>
+            <CameraView style={styles.camera} facing={facing} />
+            <View style={styles.gifContainer}>
+                {isVideoPlaying ? (
+                    <Text style={styles.playingMessage}>Lecture vidéo en cours...</Text>
+                ) : (
+                    <TouchableOpacity onPress={startVideoOnTV}>
+                        <Image source={require('../../assets/pres-ok-unscreen.gif')} style={styles.gif} />
+                    </TouchableOpacity>
+                )}
             </View>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    container: {
+    permissionContainer: {
         flex: 1,
-        backgroundColor: '#000',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    message: {
+        textAlign: 'center',
+        paddingBottom: 10,
+        fontSize: 18,
     },
     cameraContainer: {
         flex: 1,
@@ -186,26 +130,12 @@ const styles = StyleSheet.create({
         width: '100%',
     },
     gif: {
-        width: 500,
-        height: 500,
+        width: 200,
+        height: 200,
     },
     playingMessage: {
         fontSize: 18,
         color: 'red',
-    },
-    glView: {
-        width: 500,
-        height: 500,
-    },
-    permissionContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    message: {
-        textAlign: 'center',
-        paddingBottom: 10,
-        fontSize: 18,
     },
 });
 
